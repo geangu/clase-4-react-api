4dabf8043f82c1b11cfd82c3c6ed6e28d41c1596
Gerardo Andres Gutierrez Rodriguez <geangu@gmail.com>
2022-07-05 20:33:56 -0300

    Fix issues in courses controller

--- src/controllers/CoursesController.js
+++ src/controllers/CoursesController.js
@@ -5,9 +5,7 @@ const create = async (req, res) => {
     const { name, description, photo, price } = req.body;
     const course = new Course({ name, description, photo, price });
     await course.save();
-    res.send({
-      course,
-    });
+    res.send(course);
   } catch (e) {
     res.send({ error: e.message });
   }
@@ -15,8 +13,6 @@ const create = async (req, res) => {
 
 const read = async (req, res) => {
   try {
-    const id = req.query.id;
-
     const courses = await Course.find({});
     res.send({ courses });
   } catch (e) {
@@ -27,7 +23,7 @@ const read = async (req, res) => {
 const findById = async (req, res) => {
   try {
     const id = req.params.id;
-    const course = await Course.findOne({ id: id });
+    const course = await Course.findOne({ _id: id });
 
     if (!course) {
       res.status(404).send({
@@ -36,7 +32,7 @@ const findById = async (req, res) => {
       });
     }
 
-    res.send({ course });
+    res.send(course);
   } catch (e) {
     res.send({ error: e.message });
   }
@@ -45,7 +41,7 @@ const findById = async (req, res) => {
 const update = async (req, res) => {
   try {
     const id = req.params.id;
-    const course = await Course.findOne({ id: id });
+    const course = await Course.findOne({ _id: id });
 
     if (!course) {
       res.status(404).send({
@@ -54,14 +50,16 @@ const update = async (req, res) => {
       });
     }
 
-    const { name, description } = req.body;
+    const { name, description, photo, price } = req.body;
 
     course.name = name;
     course.description = description;
+    course.photo = photo;
+    course.price = price;
 
     await course.save();
 
-    res.send({ course });
+    res.send(course);
   } catch (e) {
     res.send({ error: e.message });
   }
@@ -70,18 +68,19 @@ const update = async (req, res) => {
 const drop = async (req, res) => {
   try {
     const id = req.params.id;
-    const course = await Course.findOne({ id: id });
+    const course = await Course.findOne({ _id: id });
 
     if (!course) {
       res.status(404).send({
         code: 404,
         message: 'Not found',
       });
+      return;
     }
 
-    await course.delete();
+    await course.deleteOne({ _id: id });
 
-    res.send({ course });
+    res.send(course);
   } catch (e) {
     res.send({ error: e.message });
   }
